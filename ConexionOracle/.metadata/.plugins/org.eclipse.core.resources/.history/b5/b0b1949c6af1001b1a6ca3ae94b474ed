4abf8598a2c14460475b98f22435646fab228337
santiago <ortiz.nestor@fuac.edu.co>
2021-07-30 14:28:58 -0500

    Update conexionPrueba

---
+++ ConexionOracle/.metadata/.plugins/org.eclipse.core.resources/.history/b5/b0b1949c6af1001b1a6ca3ae94b474ed
@@ -0,0 +1,10 @@
+package conexionOracle;
+
+public class ConexionPrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ConexionOracle/Conexion/src/conexionOracle/ConexionPrueba.java
+++ ConexionOracle/Conexion/src/conexionOracle/ConexionPrueba.java
@@ -4,7 +4,13 @@ public class ConexionPrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hola mundo Java Linus");
+		System.out.print(false);
+		System.out.print(6);
+		System.out.print("Bienvenido a git");
+		System.out.print("Mundo GIT");
+		System.out.print("Actualization second Commit");
 	}
 
 }
